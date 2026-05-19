a907b2774d788929d23aa62a87ffa3379c5eb378
Candri Panjaitan <candripanjaitan16@gmail.com>
2026-05-19 09:28:51 +0700

    Update sejarahService.js

--- src/services/sejarahService.js
+++ src/services/sejarahService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = 'https://budayabatak-backend-production.up.railway.app/api';
 
 const getAuthHeader = () => {
   const token = localStorage.getItem("adminToken");
